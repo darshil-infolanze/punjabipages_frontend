323acb2e04b20f88fe214c267add47bd54885f82
InfoLazeTech <deepakinf30@gmail.com>
2025-08-26 18:37:01 +0530

    axios config change

--- website/src/redux/axiosConfig.js
+++ website/src/redux/axiosConfig.js
@@ -2,10 +2,10 @@ import axios from "axios";
 import { store } from "./store";
 
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:3000/api/',
+  // baseURL: 'http://localhost:3000/api/',
   // baseURL: 'https://punjabipages-backend.vercel.app/api/',
   // baseURL: 'https://3.27.63.238/api/api/',
-  // baseURL: 'https://server.punjabipages.com.au/api/',
+  baseURL: 'https://server.punjabipages.com.au/api/',
   headers: {
     "Content-Type": "application/json",
   },
